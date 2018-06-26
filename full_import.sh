352f79eada08994cfce3024c67029669599ddcb0
Kai Blin <kblin@biosustain.dtu.dk>
2018-06-26 16:50:26 +0200

    full_import: Keep track of which files were already loaded to speed up the import
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ full_import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+touch imported.txt
+
+readonly SEQDIR=$1
+
+for infile in $(find ${SEQDIR} -name "*.final.gbk"); do
+    grep -q ${infile} imported.txt && echo "Skipping ${infile}" && continue
+    echo ${infile}
+    ./import_genbank.py ${infile}
+    echo ${infile} >> imported.txt
+done
