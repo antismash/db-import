b3f6200b83491368e8aaf25c73bceee0db7e959f
Simon Shaw <SJShaw@users.noreply.github.com>
2023-05-09 15:26:40 +0200

    chore: split cross-link sections into a second, deferred stage

--- full_import.sh
+++ full_import.sh
@@ -5,6 +5,7 @@ set -o pipefail
 set -o nounset
 
 touch imported.txt
+touch deferred_imported.txt
 
 readonly SEQDIR=$1
 
@@ -13,4 +14,11 @@ for infile in $(find ${SEQDIR} -name "*.json"); do
     echo ${infile}
     ./import_json.py ${infile}
     echo ${infile} >> imported.txt
+done
+
+for infile in $(find ${SEQDIR} -name "*.json"); do
+    grep -q ${infile} deferred_imported.txt && echo "Skipping ${infile}" && continue
+    echo ${infile}
+    ./import_deferred.py ${infile}
+    echo ${infile} >> deferred_imported.txt
 done
---
+++ import_deferred.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+""" Import the sections of antiSMASH results for a record that contain crosslinks
+    to other records within the database, requiring them to have been already
+    committed.
+"""
+from argparse import ArgumentParser
+import json
+import os
+import time
+import traceback
+
+# pylint: disable=line-too-long,missing-docstring
+
+import antismash
+from antismash.common.secmet import Record
+import psycopg2
+import psycopg2.extensions
+
+from dbimporter.common.record_data import RecordData
+from dbimporter.common import (
+    comparippson,
+    getters,
+    preparation,
+)
+from dbimporter.modules import (
+    clusterblast,
+)
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
+DB_CONNECTION = "host='localhost' port=5432 user='postgres' password='secret' dbname='antismash'"
+REPORTED_TYPES = set()
+
+DEFAULT_AS_OPTIONS = antismash.config.build_config(["--minimal"], modules=antismash.main.get_all_modules())
+DEFAULT_AS_OPTIONS.all_enabled_modules = []
+
+
+class ExistingRecordError(ValueError):
+    pass
+
+
+class MissingAssemblyIdError(ValueError):
+    pass
+
+
+def main(filename, db_connection):
+    """Run the import."""
+    connection = psycopg2.connect(db_connection)
+    connection.autocommit = False
+
+    with open(filename, encoding="utf-8") as handle:
+        raw_data = json.load(handle)
+    results = antismash.common.serialiser.AntismashResults.from_file(filename)
+    with connection.cursor() as cursor:
+        try:
+            assembly_id = getters.get_assembly_id(results.records[0])
+            if not assembly_id:
+                short_name, _ = os.path.splitext(os.path.basename(filename))
+                id_parts = short_name.split("_")
+                if id_parts[0] not in ("GCF", "GCA"):
+                    raise MissingAssemblyIdError("assembly ID does begin with 'GCF'/'GCA'")
+                assembly_id = "_".join(id_parts[:2])
+
+            print("assembly_id:", assembly_id, end="\t")
+            if assembly_id:
+                input_basename = os.path.basename(filename)
+                cursor.execute("SELECT (assembly_id) FROM antismash.filenames WHERE base_filename = %s AND assembly_id = %s", (input_basename, assembly_id))
+                if cursor.fetchone() is None:
+                    raise ValueError("No existing import exists to add deferred annotations to")
+            record_no = 0
+            for rec, module_results in zip(results.records, results.results):
+                raw_record = raw_data["records"][record_no]
+                record_no += 1
+                preparation.prepare_record(rec, raw_record["areas"], module_results)
+                load_deferred_sections(rec, module_results, cursor, assembly_id, record_no)
+            connection.commit()
+            print(assembly_id, "deferred changes committed", end="\t")
+        except ExistingRecordError:
+            connection.rollback()
+        except Exception:
+            connection.rollback()
+            raise
+    connection.close()
+
+
+def load_deferred_sections(rec: Record, module_results, cur, assembly_id, record_no: int):
+    """Import deferred portions of records."""
+    if not rec.get_regions():
+        return
+    seq_id = rec.annotations['accessions'][0]
+
+    data = RecordData(cur, rec, seq_id, assembly_id, module_results, record_no)
+
+    for region in sorted(rec.get_regions()):
+        handle_region(data, seq_id, region)
+
+    comparippson.import_results(data)
+
+
+def handle_region(data: RecordData, sequence_id, region):
+    """Import deferred portions of regions."""
+    assert region
+    data.current_region = region
+
+    clusterblast.import_region_results(data, region)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--db', default=DB_CONNECTION, help="DB connection string to use (default: %(default)s)")
+    parser.add_argument('filenames', nargs="*")
+    args = parser.parse_args()
+    total_duration = 0
+    total_imports = 0
+    successful_imports = 0
+    for filename in args.filenames:
+        start_time = time.time()
+        try:
+            main(filename, args.db)
+            successful_imports += 1
+        except MissingAssemblyIdError as err:
+            print("failed to import", filename, ":", err)
+        except Exception as err:
+            print("failed to import", filename, ":", err)
+            traceback.print_exc()
+        finally:
+            end_time = time.time()
+            import_duration = end_time - start_time
+            print("took", round(import_duration, 2), "seconds", end="\t")
+            total_imports += 1
+            total_duration += import_duration
+            print("average:", round(total_duration/total_imports, 2), f"for {total_imports} total imports ({successful_imports} successful)")
